01194dc3ad384b11d91da9fdde27cd80180967d3
Eamonn de Leastar <edeleastar@wit.ie>
2016-10-13 12:30:22 +0100

    simplified assets server pattern

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,3 @@
-const Assets = require('./controllers/assets');
 const Application = require('./controllers/application');
 
 module.exports = [
@@ -6,10 +5,6 @@ module.exports = [
   { method: 'GET', path: '/', config: Application.index },
   { method: 'GET', path: '/about', config: Application.about },
 
-  {
-    method: 'GET',
-    path: '/{param*}',
-    config: { auth: false },
-    handler: Assets.servePublicDirectory,
-  },
+  { method: 'GET', path: '/{param*}', config: { auth: false }, handler: { directory: { path: 'public', } }, },
 ];
+
